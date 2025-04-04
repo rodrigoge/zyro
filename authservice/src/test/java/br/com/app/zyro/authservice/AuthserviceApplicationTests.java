d454219720962eb2ef8758d7d498350c54fdeb90
Rodrigo Gouveia <rgestevao@gmail.com>
2025-04-04 20:38:28 -0300

    feat: creating auth-service

---
+++ authservice/src/test/java/br/com/app/zyro/authservice/AuthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app.zyro.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
